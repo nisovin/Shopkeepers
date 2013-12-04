d8927e0e7a5559dd41980dba84428a3d6d38bb53
blablubbabc <lukas@wir-sind-wir.de>
2013-12-04 21:11:34 +0100

    Added new optional feature (default: enabled): If 'bypass-spawn-blocking' is enabled, it will now try to "force-spawn" LivingEntity-Shopkeepers. This should fix most issues with Shopkeeper-Entities not being spawned due to protection plugins blocking creature spawning. For example: WorldGuard's deny mob-spawning flag is now bypassed.

--- src/com/nisovin/shopkeepers/Settings.java
+++ src/com/nisovin/shopkeepers/Settings.java
@@ -10,6 +10,7 @@ public class Settings {
 	public static boolean disableOtherVillagers = true;
 	public static boolean blockVillagerSpawns = false;
 	public static boolean enableSpawnVerifier = false;
+	public static boolean bypassSpawnBlocking = true;
 	public static boolean enablePurchaseLogging = false;
 	public static boolean saveInstantly = true;
 	
