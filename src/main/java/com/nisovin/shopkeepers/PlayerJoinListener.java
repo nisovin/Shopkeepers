e23e3a54c4dc4e3bb6ce324659197748ba04e7be
blablubbabc <lukas@wirsindwir.de>
2014-04-14 04:58:20 +0200

    Move update logic out the PlayerShopkeeper and JoinListener into the main plugin class.

--- src/main/java/com/nisovin/shopkeepers/PlayerJoinListener.java
+++ src/main/java/com/nisovin/shopkeepers/PlayerJoinListener.java
@@ -1,39 +1,14 @@
 package com.nisovin.shopkeepers;
 
-import java.util.List;
-
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import com.nisovin.shopkeepers.shoptypes.PlayerShopkeeper;
-
 public class PlayerJoinListener implements Listener {
-
-	public PlayerJoinListener() {
-		// let's update the shopkeepers for all online players:
-		for (Player player : Bukkit.getOnlinePlayers()) {
-			this.updateShopkeepersForPlayer(player);;
-		}
-	}
 	
 	@EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerJoin(PlayerJoinEvent event) {
-		this.updateShopkeepersForPlayer(event.getPlayer());
-	}
-	
-	// checks for missing owner uuids and updates owner names:
-	private void updateShopkeepersForPlayer(Player player) {
-		for (List<Shopkeeper> shopkeepers : ShopkeepersPlugin.getInstance().allShopkeepersByChunk.values()) {
-			for (Shopkeeper shopkeeper : shopkeepers) {
-				if (shopkeeper instanceof PlayerShopkeeper) {
-					PlayerShopkeeper playerShop = (PlayerShopkeeper) shopkeeper;
-					playerShop.updateOnPlayerJoin(player);
-				}
-			}
-		}
+		ShopkeepersPlugin.getInstance().updateShopkeepersForPlayer(event.getPlayer());
 	}
 }
