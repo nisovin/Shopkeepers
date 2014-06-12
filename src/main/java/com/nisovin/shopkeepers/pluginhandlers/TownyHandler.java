102159402b5adb065b583aa2497845e8a4dd37d4
blablubbabc <lukas@wirsindwir.de>
2014-06-12 03:18:43 +0200

    Checking if towny plugin is even there before attempting to use it.

--- src/main/java/com/nisovin/shopkeepers/pluginhandlers/TownyHandler.java
+++ src/main/java/com/nisovin/shopkeepers/pluginhandlers/TownyHandler.java
@@ -1,6 +1,8 @@
 package com.nisovin.shopkeepers.pluginhandlers;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.plugin.Plugin;
 
 import com.palmergames.bukkit.towny.object.TownBlock;
 import com.palmergames.bukkit.towny.object.TownBlockType;
@@ -9,8 +11,12 @@ import com.palmergames.bukkit.towny.object.TownyUniverse;
 public class TownyHandler {
 
 	public static boolean isCommercialArea(Location loc) {
-		TownBlock townBlock = TownyUniverse.getTownBlock(loc);
-		return townBlock.getType() == TownBlockType.COMMERCIAL;
+		Plugin plugin = Bukkit.getPluginManager().getPlugin("Towny");
+		if (plugin != null) {
+			TownBlock townBlock = TownyUniverse.getTownBlock(loc);
+			return townBlock.getType() == TownBlockType.COMMERCIAL;
+		} else {
+			return false;
+		}
 	}
-
 }
