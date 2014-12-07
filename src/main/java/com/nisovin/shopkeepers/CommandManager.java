ce66ac9d70f11f13f508ccedfd0a68bad546d478
blablubbabc <lukas@wirsindwir.de>
2014-12-07 04:34:08 +0100

    Fixed error due to missing brackets.

--- src/main/java/com/nisovin/shopkeepers/CommandManager.java
+++ src/main/java/com/nisovin/shopkeepers/CommandManager.java
@@ -44,7 +44,7 @@ class CommandManager implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		if (args.length > 0 && args[0].equalsIgnoreCase("help") || args[0].equals("?")) {
+		if (args.length > 0 && (args[0].equalsIgnoreCase("help") || args[0].equals("?"))) {
 			if (!sender.hasPermission(ShopkeepersPlugin.HELP_PERMISSION)) {
 				Utils.sendMessage(sender, Settings.msgNoPermission);
 				return true;
