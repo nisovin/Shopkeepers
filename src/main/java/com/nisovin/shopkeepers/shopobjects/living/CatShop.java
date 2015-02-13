d4ae6522460d3b362fee5d4b19517649f0af6090
blablubbabc <lukas@wirsindwir.de>
2015-02-13 03:55:08 +0100

    Added: one can now switch between different cat types for the ocelot shopkeeper.

---
+++ src/main/java/com/nisovin/shopkeepers/shopobjects/living/CatShop.java
@@ -0,0 +1,84 @@
+package com.nisovin.shopkeepers.shopobjects.living;
+
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Ocelot;
+import org.bukkit.inventory.ItemStack;
+
+import com.nisovin.shopkeepers.ShopCreationData;
+import com.nisovin.shopkeepers.Shopkeeper;
+
+public class CatShop extends LivingEntityShop {
+
+	private Ocelot.Type catType = Ocelot.Type.WILD_OCELOT;
+
+	protected CatShop(Shopkeeper shopkeeper, ShopCreationData creationData, LivingEntityType livingType) {
+		super(shopkeeper, creationData, livingType);
+	}
+
+	@Override
+	protected void load(ConfigurationSection config) {
+		super.load(config);
+		String catTypeName = config.getString("catType");
+		try {
+			catType = Ocelot.Type.valueOf(catTypeName);
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	protected void save(ConfigurationSection config) {
+		super.save(config);
+		config.set("skeletonType", catType.name());
+	}
+
+	@Override
+	public boolean spawn() {
+		boolean spawned = super.spawn();
+		if (spawned && entity != null && entity.isValid()) {
+			this.applySubType();
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private void applySubType() {
+		if (entity == null || !entity.isValid()) return;
+		assert entity.getType() == EntityType.OCELOT;
+		((Ocelot) entity).setCatType(catType);
+	}
+
+	@Override
+	public ItemStack getSubTypeItem() {
+		return new ItemStack(Material.WOOL, 1, this.getSubItemData(catType));
+	}
+
+	@Override
+	public void cycleSubType() {
+		int id = catType.getId();
+		catType = Ocelot.Type.getType(++id);
+		if (catType == null) {
+			catType = Ocelot.Type.WILD_OCELOT; // id 0
+		}
+		assert catType != null;
+		this.applySubType();
+	}
+
+	private short getSubItemData(Ocelot.Type catType) {
+		switch (catType) {
+		case BLACK_CAT:
+			return DyeColor.BLACK.getWoolData();
+		case RED_CAT:
+			return DyeColor.RED.getWoolData();
+		case SIAMESE_CAT:
+			return DyeColor.GRAY.getWoolData();
+
+		case WILD_OCELOT:
+		default:
+			return DyeColor.ORANGE.getWoolData();
+		}
+	}
+}
