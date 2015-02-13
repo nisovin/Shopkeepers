401e33dcf66089b214e5305fd6c4ffa00a70dc1a
blablubbabc <lukas@wirsindwir.de>
2015-02-13 03:10:58 +0100

    Added: Skeleton shopkeepers can now be switched to wither skeletons.

---
+++ src/main/java/com/nisovin/shopkeepers/shopobjects/living/SkeletonShop.java
@@ -0,0 +1,76 @@
+package com.nisovin.shopkeepers.shopobjects.living;
+
+import org.bukkit.Material;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Skeleton;
+import org.bukkit.entity.Skeleton.SkeletonType;
+import org.bukkit.inventory.ItemStack;
+
+import com.nisovin.shopkeepers.ShopCreationData;
+import com.nisovin.shopkeepers.Shopkeeper;
+
+public class SkeletonShop extends LivingEntityShop {
+
+	private SkeletonType skeletonType = SkeletonType.NORMAL;
+
+	protected SkeletonShop(Shopkeeper shopkeeper, ShopCreationData creationData, LivingEntityType livingType) {
+		super(shopkeeper, creationData, livingType);
+	}
+
+	@Override
+	protected void load(ConfigurationSection config) {
+		super.load(config);
+		String skeletonTypeName = config.getString("skeletonType");
+		try {
+			skeletonType = SkeletonType.valueOf(skeletonTypeName);
+		} catch (Exception e) {
+		}
+	}
+
+	@Override
+	protected void save(ConfigurationSection config) {
+		super.save(config);
+		config.set("skeletonType", skeletonType.name());
+	}
+
+	@Override
+	public boolean spawn() {
+		boolean spawned = super.spawn();
+		if (spawned && entity != null && entity.isValid()) {
+			assert entity.getType() == EntityType.SKELETON;
+			((Skeleton) entity).setSkeletonType(skeletonType);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public ItemStack getSubTypeItem() {
+		return new ItemStack(Material.SKULL_ITEM, 1, this.getSkullItemData(skeletonType));
+	}
+
+	@Override
+	public void cycleSubType() {
+		int id = skeletonType.getId();
+		skeletonType = SkeletonType.getType(++id);
+		if (skeletonType == null) {
+			skeletonType = SkeletonType.NORMAL;
+		}
+
+		assert entity.getType() == EntityType.SKELETON;
+		((Skeleton) entity).setSkeletonType(skeletonType);
+	}
+
+	private short getSkullItemData(SkeletonType skeletonType) {
+		switch (skeletonType) {
+		case WITHER:
+			return 1;
+
+		case NORMAL:
+		default:
+			return 0;
+		}
+	}
+}
