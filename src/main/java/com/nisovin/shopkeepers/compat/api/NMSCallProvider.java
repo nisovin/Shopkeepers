9002f07223225fc7416d902be5269674a3afb1c6
blablubbabc <lukas@wirsindwir.de>
2015-05-24 19:12:52 +0200

    Removed no longer used code from nms modules.

--- src/main/java/com/nisovin/shopkeepers/compat/api/NMSCallProvider.java
+++ src/main/java/com/nisovin/shopkeepers/compat/api/NMSCallProvider.java
@@ -39,41 +39,6 @@ public interface NMSCallProvider {
 
 	public String saveItemAttributesToString(ItemStack item);
 
-	/**
-	 * Compares the attribute types and modifiers of the given items.
-	 * Does ignore the specific attribute uuids.
-	 * 
-	 * @param item1
-	 *            A non-null itemstack.
-	 * @param item2
-	 *            A non-null itemstack.
-	 * @return
-	 */
-	// public boolean areAttributesSimilar(ItemStack item1, ItemStack item2);
-
-	/**
-	 * Performs additional version specific general item comparison.
-	 * Item meta comparison is handled separately.
-	 * It can be asserted that all other general item comparison was run before
-	 * and that the given items are both not null.
-	 * 
-	 * @param item1
-	 * @param item2
-	 * @return A reason why the given items are not similar, otherwise null.
-	 */
-	// public String areSimilarReasoned(ItemStack item1, ItemStack item2);
-
-	/**
-	 * Performs additional version specific item meta comparison.
-	 * It can be asserted that all other general item comparison was run before
-	 * and that the given item meta are both of the same type and not null.
-	 * 
-	 * @param itemMeta1
-	 * @param itemMeta2
-	 * @return A reason why the given item meta are not similar, otherwise null.
-	 */
-	// public String areSimilarReasoned(ItemMeta itemMeta1, ItemMeta itemMeta2);
-
 	public boolean supportsPlayerUUIDs();
 
 	public UUID getUUID(OfflinePlayer player);
