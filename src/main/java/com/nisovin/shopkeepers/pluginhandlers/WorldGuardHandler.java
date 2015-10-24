b6181e6b7b60e87143f6b7d0842bf3fc6482a535
blablubbabc <lukas@wirsindwir.de>
2015-10-24 03:07:51 +0200

    Improved WorldGuard hook:
    Players are now also allowed to place a shopkeeper if the region they
    are in has WorldGuard's ALLOW_SHOP flag set (regardless if they can
    build in that region).
    
    Updated WorldGuard dependency to 5.9, added dependency to WorldEdit 5.6
    (otherwise it wouldn't let me compile..) and added sk89q's official
    maven repository.

--- src/main/java/com/nisovin/shopkeepers/pluginhandlers/WorldGuardHandler.java
+++ src/main/java/com/nisovin/shopkeepers/pluginhandlers/WorldGuardHandler.java
@@ -6,13 +6,23 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.DefaultFlag;
 
 public class WorldGuardHandler {
 
-	public static boolean canBuild(Player player, Location loc) {
-		Plugin plugin = Bukkit.getPluginManager().getPlugin("WorldGuard");
+	private static final String PLUGIN_NAME = "WorldGuard";
+
+	public static Plugin getPlugin() {
+		return Bukkit.getPluginManager().getPlugin(PLUGIN_NAME);
+	}
+
+	public static boolean isShopAllowed(Player player, Location loc) {
+		Plugin plugin = getPlugin();
 		if (plugin != null) {
-			return ((WorldGuardPlugin) plugin).canBuild(player, loc);
+			WorldGuardPlugin wgPlugin = (WorldGuardPlugin) plugin;
+			// allow shops in regions where the player can build or where the ENABLE_SHOP flag is set:
+			return wgPlugin.canBuild(player, loc)
+					|| wgPlugin.getRegionManager(loc.getWorld()).getApplicableRegions(loc).allows(DefaultFlag.ENABLE_SHOP);
 		} else {
 			return true;
 		}
